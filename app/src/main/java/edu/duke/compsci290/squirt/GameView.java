b3c019a2666a69277c4bc1bc224dad16558c8ddb
Elizabeth Bartusiak <eeb50@duke.edu>
2018-05-14 18:19:28 -0400

    put canvas in app

---
+++ app/src/main/java/edu/duke/compsci290/squirt/GameView.java
@@ -0,0 +1,69 @@
+package edu.duke.compsci290.squirt;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.MainThread;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Elizabeth on 5/14/2018.
+ */
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private myMainThread mThread;
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+        mThread = new myMainThread(getHolder(), this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mThread.setRunning(true);
+        mThread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                mThread.setRunning(false);
+                mThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    public void update(){
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (canvas != null) {
+            canvas.drawColor(Color.WHITE);
+            Paint paint = new Paint();
+            paint.setColor(Color.rgb(250, 0, 0));
+            canvas.drawRect(100, 100, 200, 200, paint);
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/edu/duke/compsci290/squirt/myMainThread.java
@@ -0,0 +1,53 @@
+package edu.duke.compsci290.squirt;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Elizabeth on 5/14/2018.
+ */
+
+public class myMainThread extends Thread {
+
+    private SurfaceHolder mSurfaceHolder;
+    private GameView mGameView;
+    private boolean mIsRunning;
+    public static Canvas mCanvas;
+
+    public myMainThread(SurfaceHolder surfaceHolder, GameView gameView){
+        super();
+        mSurfaceHolder = surfaceHolder;
+        mGameView = gameView;
+    }
+
+    @Override
+    public void run() {
+        while (mIsRunning) {
+            mCanvas = null;
+            try {
+                mCanvas = this.mSurfaceHolder.lockCanvas();
+                synchronized(mSurfaceHolder) {
+                    this.mGameView.update();
+                    this.mGameView.draw(mCanvas);
+                }
+            } catch (Exception e) {} finally {
+                if (mCanvas != null) {
+                    try {
+                        mSurfaceHolder.unlockCanvasAndPost(mCanvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public void setRunning(boolean isRunning) {
+        mIsRunning = isRunning;
+    }
+
+
+
+
+
+}
